c79e9ce2b49c807558b2c762905a59eb734166eb
johncausey <martin.causey@gmail.com>
2013-12-29 04:43:24 -0700

    constant tabs and spaces

--- main.c
+++ main.c
@@ -1,28 +1,34 @@
+/*
+	Program to read a file
+	and count newlines, tabs,
+	and spaces.
+*/
 
 #include <stdio.h>
 
 main()
 {
-  FILE *ff;
-  int c, tb, bl, nl;
-  tb = bl = nl = 0;
+	FILE *ff; /* Setting file */
+	int c, tb, bl, nl; /* Setting int */
+	tb = bl = nl = 0; /* Values should equal 0 before while statement - counter values */
 
-  ff = fopen("contentfile.txt", "r");
-  if (ff) {
-	while ((c = getc(ff)) != EOF) {
-	  if (c == '\t')
-		++tb;
-	  if (c == ' ')
-		++bl;
-	  if (c == '\n')
-		++nl;
-	  putchar(c);
+	ff = fopen("contentfile.txt", "r"); /* Read file */
+	if (ff) {
+		while ((c = getc(ff)) != EOF) {
+			/*  */
+			if (c == '\t')
+				++tb;
+			if (c == ' ')
+				++bl;
+			if (c == '\n')
+				++nl;
+			putchar(c);
+		}
+		fclose(ff);
+		printf("There are %d tabs, %d blanks, and %d newlines in file.\n", tb, bl, nl);
 	}
-	fclose(ff);
-	printf("There are %d tabs, %d blanks, and %d newlines in file.\n", tb, bl, nl);
-  }
-  if (!ff) {
-	printf("Unable to open file\n");
-	return 1;
-  }
+	if (!ff) {
+		printf("Unable to open file\n");
+		return 1;
+	}
 }
