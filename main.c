6e7a1bcb860048327277a159aaa27b7c262a9def
johncausey <martin.causey@gmail.com>
2014-01-05 10:15:58 -0700

    start tail program

--- main.c
+++ main.c
@@ -1,19 +1,42 @@
-// Program to do stuff but not sure what yet.
+// Program to return the last 10 lines of a file.
 #include <stdio.h>
 
+int smfile();
+int lafile(int *);
+
 int main( int argc, char ** argv ) {
-	FILE *ff; // Setting file
-	int c; // Setting int
+	FILE *ff;
+	int c;
+	int nn = 0;
+
 
 	ff = fopen("contentfile.txt", "r"); // Open file.
+	// File exists.
 	if (ff) {
 		while ((c = getc(ff)) != EOF) {
-			putchar(c); //Show file contents.
+			if(c == '\n') {
+				++nn;
+			}
+			//putchar(c); //Show file contents.
 		}
+		if((nn <= 10)?(smfile()):(lafile(&nn)));
 		fclose(ff); // Close file.
 	}
+	// File does not exist.
 	if (!ff) {
-		printf("Unable to open file\n"); // Error if no file.
+		printf("Unable to open file.\n"); // Error if no file.
 		return 1;
 	}
+}
+
+
+int smfile() {
+	printf("Print all lines option (10 or less).\n");
+	return 0;
+}
+
+
+int lafile(int *nn) {
+	printf("Print trailing lines, it has %d lines total.\n", *nn);
+	return 0;
 }
