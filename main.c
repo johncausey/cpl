c7c782e29a6902d30ab623b2eb8d167bb0a00803
johncausey <martin.causey@gmail.com>
2014-01-04 22:22:03 -0700

    change comments and main

--- main.c
+++ main.c
@@ -1,21 +1,17 @@
-/*
-	Program to read a file
-	and count newlines, tabs,
-	and spaces.
-*/
+// Program to read a file and
+// count newlines, tabs, and spaces.
 
 #include <stdio.h>
 
-main()
-{
-	FILE *ff; /* Setting file */
-	int c, tb, bl, nl; /* Setting int */
-	tb = bl = nl = 0; /* Values should equal 0 before while statement - counter values */
+int main( int argc, char ** argv ) {
+	FILE *ff; // Setting file
+	int c, tb, bl, nl; // Setting int
+	tb = bl = nl = 0; // Values should equal 0 before while statement - counter values
 
-	ff = fopen("contentfile.txt", "r"); /* Read file */
+	ff = fopen("contentfile.txt", "r"); // Read file
 	if (ff) {
 		while ((c = getc(ff)) != EOF) {
-			/* Read each character, putchar all and count nl, tb, bl */
+			// Read each character, putchar all and count nl, tb, bl
 			if (c == '\t')
 				++tb;
 			if (c == ' ')
@@ -24,11 +20,11 @@ main()
 				++nl;
 			putchar(c);
 		}
-		/* Close file and present results */
+		// Close file and present results
 		fclose(ff);
 		printf("\nThere are %d tabs, %d blanks, and %d newlines in file.\n", tb, bl, nl);
 	}
-	/* Present error message if file fails to be opened/found */
+	// Present error message if file fails to be opened/found
 	if (!ff) {
 		printf("Unable to open file\n");
 		return 1;
