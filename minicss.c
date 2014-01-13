57c3222815695af4f423be72932e83fe8f8873e2
johncausey <martin.causey@gmail.com>
2014-01-13 02:43:29 -0700

    add space duplication ignore

--- minicss.c
+++ minicss.c
@@ -2,17 +2,28 @@
 
 #include <stdio.h>
 
+#define INSPACE 1    // Inside a space grouping.
+#define OUTSPACE 0   // Outside a space grouping.
+
 void main( int argc, char ** argv ) {
 	
 	FILE * fr = fopen(argv[1], "r");
 	FILE * fw = fopen("minified.css", "w");
-	int c;
-	int i = 0;
+	int c, i = 0;
+	int space_s = OUTSPACE;
 
 	if (fr) {
 		while ((c = getc(fr)) != EOF ) {
 			if ((c != '\n') && (c != '\t')) {
-				putc(c, fw);
+				if ((c == ' ') && (space_s == OUTSPACE)) {
+					putc(c, fw);
+					space_s = INSPACE;
+				} else if ((c == ' ') && (space_s == INSPACE)) {
+					;
+				} else if (c != ' ') {
+					space_s = OUTSPACE;
+					putc(c, fw);
+				}
 			}
 		}
 		printf("Minification Complete.\n");
