f172e7f1406386f63ba7ef1fb2389f9c36f8ba2f
johncausey <martin.causey@gmail.com>
2014-01-29 05:31:49 -0700

    start tictac

---
+++ tictac.c
@@ -0,0 +1,30 @@
+// Tic Tac Toe game in C with computer player
+
+#include <stdio.h>
+
+#define PM 1 // Player mark of X
+#define CM 2 // Computer mark of O
+
+
+int print_board(int*, int);
+
+
+int main(void) {
+	int board[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // Initialize game board
+	print_board(board, 9);
+	return 0;
+}
+
+
+int print_board(int board[], int size) {	
+	int n;
+	for(n = 0; n < size; n++ ) {
+		if(n % 3 == 0) {
+			printf("\n\t");
+		}
+		printf("%d  ", board[n]);
+		if(n == 8) {
+			printf("\n\n");
+		}
+	}
+}
