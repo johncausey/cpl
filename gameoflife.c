777bdcbc962e36da5e3fa5e69fbdbd955ed0bf0c
johncausey <martin.causey@gmail.com>
2014-01-30 20:42:28 -0700

    clean up

--- gameoflife.c
+++ gameoflife.c
@@ -3,20 +3,26 @@
 #include <string.h> // memset
 #include <unistd.h> // usleep
 
+#define LIFE '0'
+#define DEATH ' '
+#define ROWSIZE 30
+#define COLSIZE 70
+
 int main() {
-	int row = 30;
-	int col = 70;
+	// Initialize simulation area
+	int row = ROWSIZE;
+	int col = COLSIZE;
 	int grid[row][col];
-	memset(grid, '.', row*col*sizeof(int));
+	memset(grid, DEATH, row*col*sizeof(int));
 	srand(time(NULL));
 
 	// Start simulation.
 	int n, i, c, t;
 	for (t = 0; t < 1000; t++) {
 		initscr();
-		int random_row = rand() % 30;
-		int random_col = rand() % 70;
-		grid[random_row][random_col] = '1';
+		int random_row = rand() % row;
+		int random_col = rand() % col;
+		grid[random_row][random_col] = LIFE;
 
 		for (i = 0; i < row; i++) {
 			for (c = 0; c < col; c++) {
@@ -26,11 +32,13 @@ int main() {
 		}
 
 		// Finish window and refresh
-		printw("\nGame of Life\n\n");
+		printw("\nGame of Life - No random variants\n\n");
 		refresh();
 		clear();
 		usleep(30000);
 	}
+
+	// Close window and return to terminal
 	endwin();
 	return 0;
 }
